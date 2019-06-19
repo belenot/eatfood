bd6d62787cdeeeaaa4853d910e423641d272ebac
belenot <kholhunov0@gmail.com>
2019-06-19 10:08:03 +0300

    A little improvement of AccessedInterceptor

--- src/main/java/com/belenot/eatfood/web/interceptor/SessionInterceptor.java
+++ src/main/java/com/belenot/eatfood/web/interceptor/SessionInterceptor.java
@@ -1,5 +1,7 @@
 package com.belenot.eatfood.web.interceptor;
 
+import java.io.IOException;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
@@ -8,13 +10,21 @@ import org.springframework.web.servlet.HandlerInterceptor;
 
 public class SessionInterceptor implements HandlerInterceptor {
     @Override
-    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
-	String uri = request.getRequestURI().split("/")[1];
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws IOException {
+	String uri = request.getRequestURI().split("/")[2];
 	HttpSession session = request.getSession(false);
-	if (uri.equals("foodlist") && session == null) {
+	boolean accessed = false;
+	try {
+	    accessed = session == null ? false : session.getAttribute("client") != null;
+	} catch (Exception exc) { accessed = false; }
+	if (uri.equals("foodlist") && !accessed) {
+	    response.sendRedirect("registration");
 	    return false;
 	}
-	
+	if ((uri.equals("registration") || uri.equals("authorization")) && accessed) {
+	    response.sendRedirect("foodlist");
+	    return false;
+	}
 	return true;
     }
 }
