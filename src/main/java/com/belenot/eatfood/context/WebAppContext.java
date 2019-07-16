ffdd1d6a11f98ec2b6a0ff9df0b6a6f377fa5f3d
belenot <kholhunov0@gmail.com>
2019-07-16 18:28:57 +0300

    version2

---
+++ src/main/java/com/belenot/eatfood/context/WebAppContext.java
@@ -0,0 +1,156 @@
+package com.belenot.eatfood.context;
+
+import java.nio.charset.StandardCharsets;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+import java.util.Locale;
+
+import com.belenot.eatfood.context.aspect.CritErrorAspect;
+import com.belenot.eatfood.context.aspect.LoggingAspect;
+import com.belenot.eatfood.dao.ClientDao;
+import com.belenot.eatfood.dao.ClientDaoSql;
+import com.belenot.eatfood.dao.DoseDao;
+import com.belenot.eatfood.dao.DoseDaoSql;
+import com.belenot.eatfood.dao.FoodDao;
+import com.belenot.eatfood.dao.FoodDaoSql;
+import com.belenot.eatfood.service.DaoService;
+import com.belenot.eatfood.service.DaoServiceImpl;
+import com.belenot.eatfood.web.interceptor.EncodingInterceptor;
+import com.belenot.eatfood.web.interceptor.SessionInterceptor;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.core.env.Environment;
+import org.springframework.format.Formatter;
+import org.springframework.format.FormatterRegistry;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.StringHttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.web.context.ConfigurableWebApplicationContext;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@Configuration
+@EnableWebMvc
+@EnableAspectJAutoProxy
+@ComponentScan( "com.belenot.eatfood.aspect, com.belenot.eatfood.web" )
+@PropertySource( "classpath:server.properties" )
+public class WebAppContext implements WebMvcConfigurer {
+
+    @Autowired
+    public ConfigurableWebApplicationContext ctx;
+
+    @Autowired
+    public Environment env;
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+	registry.addResourceHandler("/resources/**").addResourceLocations(env.getProperty("server.resources.path", "/resources/"));
+    }
+    
+    @Bean
+    public ViewResolver viewResolver() {
+	ViewResolver viewResolver = new InternalResourceViewResolver("/WEB-INF/view/", ".jsp");
+	return viewResolver;
+    }
+    @Bean
+    public DaoService daoService() {
+	DaoServiceImpl daoServiceImpl = new DaoServiceImpl();
+	daoServiceImpl.setClientDao(clientDao());
+	daoServiceImpl.setFoodDao(foodDao());
+	daoServiceImpl.setDoseDao(doseDao());
+	return daoServiceImpl;
+    }
+    @Bean
+    public JdbcConnectionFactory jdbcConnectionFactory() {
+	JdbcConnectionFactory jdbcConnectionFactory = new JdbcConnectionFactory();
+	jdbcConnectionFactory.setAddress("jdbc:postgresql://localhost:8832/eatfood2?currentSchema=v0_2");
+	jdbcConnectionFactory.setUsername("eatfood");
+	jdbcConnectionFactory.setPassword("eatfood");
+	return jdbcConnectionFactory;
+    }
+    @Bean
+    public ClientDao clientDao() {
+	ClientDaoSql clientDao = new ClientDaoSql();
+	clientDao.setConnection(jdbcConnectionFactory().getConnection());
+	return clientDao;
+    }
+    @Bean
+    public FoodDao foodDao() {
+	FoodDaoSql foodDao = new FoodDaoSql();
+	foodDao.setClientDao(clientDao());
+	foodDao.setConnection(jdbcConnectionFactory().getConnection());
+	return foodDao;
+    }
+    @Bean
+    public DoseDao doseDao() {
+	DoseDaoSql doseDao = new DoseDaoSql();
+	doseDao.setFoodDao(foodDao());
+	doseDao.setConnection(jdbcConnectionFactory().getConnection());
+	return doseDao;
+    }
+    @Bean
+    public Logger logger() {
+	Logger logger = LogManager.getLogger();
+	return logger;
+    }
+    @Bean
+    public CritErrorAspect critErrorAspect() {
+	CritErrorAspect critErrorAspect = new CritErrorAspect();
+	critErrorAspect.setCtx(ctx);
+	return new CritErrorAspect();
+    }
+    @Bean
+    public LoggingAspect loggingAspect() {
+	LoggingAspect loggingAspect = new LoggingAspect();
+	return loggingAspect;
+    }
+    @Bean
+    public MessageSource messageSource() {
+        ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+	messageSource.setBasename("eatfoodFormat");
+	messageSource.setDefaultEncoding("UTF-8");
+	return messageSource;
+    }
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+	registry.addInterceptor(new EncodingInterceptor());
+	registry.addInterceptor(new SessionInterceptor());
+    }
+    @Override
+    public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+	converters.add(new StringHttpMessageConverter(StandardCharsets.UTF_8));
+	ObjectMapper objectMapper = new ObjectMapper();
+	objectMapper.setDateFormat(new SimpleDateFormat("yyyy-MM-dd"));
+	converters.add(new MappingJackson2HttpMessageConverter(objectMapper));
+    }
+    @Override
+    public void addFormatters(FormatterRegistry registry) {
+	registry.addFormatter(new Formatter<Date>() {
+		public Date parse(String str, Locale locale) {
+		    try {
+			return (new SimpleDateFormat("yyyy-MM-dd", locale)).parse(str);
+		    } catch (Exception exc) {
+			return null;
+		    }
+		}
+		public String print(Date date, Locale locale) {
+		    return (new SimpleDateFormat("yyyy-MM-dd", locale)).format(date);
+		}
+	    });
+    }
+}
